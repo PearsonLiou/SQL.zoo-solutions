66bd44cb6004eb81d4f348b7c5c92f8b929b37dd
PearsonLiou <62300111+PearsonLiou@users.noreply.github.com>
2020-04-21 00:35:37 +0800

    Update SELECT basics.sql

--- SELECT basics/SELECT basics.sql
+++ SELECT basics/SELECT basics.sql
@@ -1,7 +1,7 @@
 --SELECT basics
 
-/*
-1.The example uses a WHERE clause to show the population of 'France'.
+/* 1
+The example uses a WHERE clause to show the population of 'France'.
 Note that strings (pieces of text that are data) should be in 'single quotes';
 Modify it to show the population of Germany
 */
@@ -9,8 +9,8 @@ SELECT population FROM world
   WHERE name = 'Germany'
 
 
-/*
-2.Checking a list The word IN allows us to check if an item is in a list.
+/* 2
+Checking a list The word IN allows us to check if an item is in a list.
 The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
 Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 */
@@ -18,7 +18,7 @@ SELECT name, population FROM world
   WHERE name IN ('Sweden','Norway','Denmark');
 
 
-/*
+/* 3
 Which countries are not too small and not too big? 
 BETWEEN allows range checking (range specified is inclusive of boundary values).
 The example below shows countries with an area of 250,000-300,000 sq. km. 
